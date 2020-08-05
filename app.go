4cb939aebd7346d01942aecc5fe67860262c43c7
Alex Bates <alex@hciyork.com>
2020-08-05 16:26:20 +0100

    working api call with error detection

--- app.go
+++ app.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -12,11 +13,25 @@ func init() {
 	log.SetReportCaller(true)
 }
 
-func GetIlkleyWeather() {
+// func GetIlkleyWeather() {
 
-}
+// }
 
 func main() {
+
+	response, err := http.Get("https://api.openweathermap.org/data/2.5/weather?q=ilkley,uk&appid=11a2659c258f169b1711290aafc9c04d")
+
+	if err != nil {
+		log.Error(err.Error())
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Info(string(responseData))
+
 	log.Info("Starting WeatherApp API Server")
 	router := mux.NewRouter()
 	http.ListenAndServe(":8000", router)
