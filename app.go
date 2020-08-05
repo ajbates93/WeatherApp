8f82656530d0c5d16400777e33176495a3c0b2da
Alex Bates <alex@hciyork.com>
2020-08-05 19:53:19 +0100

    extract into function

--- app.go
+++ app.go
@@ -13,26 +13,25 @@ func init() {
 	log.SetReportCaller(true)
 }
 
-// func GetIlkleyWeather() {
-
-// }
-
-func main() {
-
+func GetIlkleyWeather(w http.ResponseWriter, r *http.Request) {
 	response, err := http.Get("https://api.openweathermap.org/data/2.5/weather?q=ilkley,uk&appid=11a2659c258f169b1711290aafc9c04d")
 
 	if err != nil {
-		log.Error(err.Error())
+		log.Error("The HTTP Request failed with error \n", err.Error())
 	}
 
 	responseData, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("The HTTP Request failed with error \n", err)
 	}
 
 	log.Info(string(responseData))
+}
+
+func main() {
 
 	log.Info("Starting WeatherApp API Server")
 	router := mux.NewRouter()
+	router.HandleFunc("/ilkley", GetIlkleyWeather).Methods("GET")
 	http.ListenAndServe(":8000", router)
 }
